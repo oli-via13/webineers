cb6844a631caaffa9e1e3096efa9669a04e7e4d8
imanuel11 <patandeanimanuel@gmail.com>
2025-05-09 17:49:57 +0800

    ubahhh

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import gambar1 from '../public/assets/diagram.png'
-import gambar2 from '../public/assets/streak.png'
-import Box from './components/box'
-import TigaBox from './components/TigaBox'
-import ListBox from './components/ListBox'
-import Test from './components/Test'
+import gambar1 from "../public/assets/diagram.png";
+import gambar2 from "../public/assets/streak.png";
+import Box from "./components/box";
+import TigaBox from "./components/TigaBox";
+import ListBox from "./components/ListBox";
+import Test from "./components/Test";
 import Komponen from "./components/Komponen";
 import { Navigasi } from "./components/Navigasi";
 import Sidebar from "./components/Sidebar";
@@ -12,10 +12,10 @@ import "./index.css";
 function App() {
   return (
     <>
-    <TigaBox/>
-    <ListBox nomor="1" kegiatan="Ujian Math" waktu="09:00 - 11:00"/>
-    <ListBox nomor="2" kegiatan="Konsultasi Unity" waktu="22:00 - 22:30"/>
-    <ListBox nomor="3" kegiatan="Date with U" waktu="199999.00"/>
+      <TigaBox />
+      <ListBox nomor="1" kegiatan="Ujian Math" waktu="09:00 - 11:00" />
+      <ListBox nomor="2" kegiatan="Konsultasi Unity" waktu="22:00 - 22:30" />
+      <ListBox nomor="3" kegiatan="Date with U" waktu="19.00" />
       <Sidebar />
       <Navigasi />
     </>
