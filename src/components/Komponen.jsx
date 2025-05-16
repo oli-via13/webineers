bb50f7f12385763f287b1155c72b1fcbfdf44987
imanuel11 <patandeanimanuel@gmail.com>
2025-05-16 17:18:19 +0800

    ima/sidebar&navigaso/kotak

--- src/components/Komponen.jsx
+++ src/components/Komponen.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const Komponen = () => {
-  return <div>Komponen</div>;
+const Komponen = (props) => {
+  return (
+    <div className="flex px-4 gap-1 items-center">
+      <img src={progres} alt="" className="w-4 h-4 " />
+      <a className="">{props.text}</a>
+    </div>
+  );
 };
 
 export default Komponen;
